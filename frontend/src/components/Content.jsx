bb5e9878c4fe57159c000ba7243731e26fd7184e
Nishant Mishra <nishant.219302089@muj.manipal.edu>
2026-01-26 23:57:56 +0530

    added paragraph

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -9,7 +9,10 @@ function Content() {
     <div className='container'>
       <div className='p-5 text-centre bg-light-dark rounded'>
         <h1 className='text-light'> Portal to Use AI Models</h1>
-        <p className='text-light lead'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati voluptates quasi doloremque, quod sint maxime accusamus nesciunt? Ut quod, quaerat minus, laudantium culpa esse, animi voluptatibus omnis cupiditate praesentium ea.</p>
+        <p className='text-light lead'>Stock price prediction model built using a Long Short-Term Memory (LSTM) neural network. The goal of this project is to understand how time-series data works and how LSTM models can be used to analyze sequential stock market data.
+
+The model uses historical stock prices to learn patterns over time and make basic future price predictions. Key steps in the project include data preprocessing, normalization, sequence creation, and training an LSTM model on past stock price data.
+</p>
         <Button text='Explore' class='btn-outline-info' url='/dashboard'/>
       </div>
       
