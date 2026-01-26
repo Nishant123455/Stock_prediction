7587991c512e6acea016aee7aeebb696315cb2e4
Nishant Mishra <nishant.219302089@muj.manipal.edu>
2026-01-27 00:22:15 +0530

    added paragraph

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <>
      <footer className='footer py-3 my-4'>
     <hr className='border-bottom' />
-    <p className='text-light text-center'>&copy; 2025 - Built with ❤️ by Nishant Mishra </p>
+    <p className='text-light text-center'>&copy; 2026 - Built with ❤️ by Nishant Mishra </p>
     </footer>
     
     </>
