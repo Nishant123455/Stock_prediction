f198752a10c3646cdec2324639ed9678a86ec875
Nishant Mishra <nishant.219302089@muj.manipal.edu>
2026-01-26 22:49:28 +0530

    correct AxiosInstance

--- frontend/src/AxiosInstance.jsx
+++ frontend/src/AxiosInstance.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_BACKEND_BASE_API
+//const baseURL = import.meta.env.VITE_BACKEND_BASE_API
+const baseURL = import.meta.env.VITE_API_BASE_URL
 const AxiosInstance = axios.create({
     baseURL: baseURL,
     headers: {
